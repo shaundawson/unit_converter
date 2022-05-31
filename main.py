2152a2d0085ceb526814d98dd111899a87e439f2
Shaun Dawson <23201454+shaundawson@users.noreply.github.com>
2022-05-31 18:43:03 -0400

    rename variables

--- main.py
+++ main.py
@@ -1,41 +1,33 @@
 from tkinter import *
 
-conversion_factor = 1.6
+# The distance in kilometers is equal to the distance in miles multiplied by 1.609344.
+conversion_factor = 1.60934
 
-def button_clicked():
-    miles = int(input.get())
-    calc = round(miles * conversion_factor,1)
+def miles_to_km():
+    miles = int(miles_input.get())
+    calc = round(miles * conversion_factor,2)
     km_output.config(text=calc)
 
 window = Tk()
 window.title("Mile to Km Converter")
-window.minsize(width=300, height=200)
 window.config(padx=20, pady=20)
 
-#Label
-main_label = Label(text="is equal to", font=("Arial", 18, "normal"))
-main_label.grid(column=0, row=1)
+is_equal_label = Label(text="is equal to", font=("Arial", 18, "normal"))
+is_equal_label.grid(column=0, row=1)
 
-#Entry
-input = Entry(width="10")
-input.grid(column=1,row=0)
+miles_input = Entry(width="5")
+miles_input.grid(column=1,row=0)
 
-# Miles Label
 miles_label = Label(text="Miles", font=("Arial", 18, "normal"))
 miles_label.grid(column=2, row=0)
 
-
-# Km output
-km_output = Label(font=("Arial", 18, "normal"))
+km_output = Label(text = "0",font=("Arial", 18, "normal"))
 km_output.grid(column=1, row=1)
 
-# Km Label
 km_label = Label(text="Km", font=("Arial", 18, "normal"))
 km_label.grid(column=2, row=1)
 
-
-#Calculate Button
-button = Button(text="Calculate", command=button_clicked)
+button = Button(text="Calculate", command=miles_to_km)
 button.grid(column=1, row=3)
 
 
